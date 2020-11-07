835ccc1700a914cee14eea519d0570c8050dc88c
Gordon <gord@ghdunker.ca>
2020-11-07 15:43:16 -0700

    A generic joomla template

---
+++ index.php
@@ -0,0 +1,89 @@
+<?php
+defined('_JEXEC') or die;
+
+$app  = JFactory::getApplication();
+$user = JFactory::getUser();
+
+// Output as HTML5
+$this->setHtml5(true);
+
+// Getting params from template
+$params = $app->getTemplate(true)->params;
+
+// Detecting Active Variables
+$option   = $app->input->getCmd('option', '');
+$view     = $app->input->getCmd('view', '');
+$layout   = $app->input->getCmd('layout', '');
+$task     = $app->input->getCmd('task', '');
+$itemid   = $app->input->getCmd('Itemid', '');
+$sitename = htmlspecialchars($app->get('sitename'), ENT_QUOTES, 'UTF-8');
+?>
+<!DOCTYPE html>
+
+<html lang="<?php echo $this->language; ?>" dir="<?php echo $this->direction; ?>">
+<head> 
+<jdoc:include type="head" />
+<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+<meta http-equiv="Content-Type" content="text/html"; charset=utf-8" />
+<meta name="description" content="Gord's Website">
+<meta name="keywords" content="radio, SWL, Geek">
+<title>VE3ZGD</title>
+<link rel="stylesheet" href="<?php echo $this->baseurl ?>/templates/gord1/css/layout.css" type="text/css" media="screen,projection">
+<script type="text/javascript" src="<?php echo $this->baseurl ?>/templates/gord1/js/test.js"></script>
+</head>
+<body>
+	
+	<div class="body" id="top">
+		<div class="container<?php echo ($params->get('fluidContainer') ? '-fluid' : ''); ?>">
+			<!-- Header -->
+			<header class="header" role="banner">
+				<div class="header-inner clearfix">
+					<a class="brand pull-left" href="<?php echo $this->baseurl; ?>/">
+						<?php echo $logo; ?>
+						<?php if ($this->params->get('sitedescription')) : ?>
+							<?php echo '<div class="site-description">' . htmlspecialchars($this->params->get('sitedescription'), ENT_COMPAT, 'UTF-8') . '</div>'; ?>
+						<?php endif; ?>
+					</a>
+					<div class="header-search pull-right">
+						<jdoc:include type="modules" name="position-0" style="none" />
+					</div>
+				</div>
+			</header>
+			      
+					<div class="nav-collapse">
+						<jdoc:include type="modules" name="position-1" style="none" />
+					</div>
+			<jdoc:include type="modules" name="banner" style="xhtml" />
+			<div class="row-fluid">
+				<?php if ($position8ModuleCount) : ?>
+					<!-- Begin Sidebar -->
+					<div id="sidebar" class="span3">
+						<div class="sidebar-nav">
+							<jdoc:include type="modules" name="position-8" style="xhtml" />
+						</div>
+					</div>
+					<!-- End Sidebar -->
+				<?php endif; ?>
+				<main id="content" role="main" class="<?php echo $span; ?>">
+					<!-- Begin Content -->
+					<jdoc:include type="modules" name="position-3" style="xhtml" />
+					<jdoc:include type="message" />
+					<jdoc:include type="component" />
+					<div class="clearfix"></div>
+					<jdoc:include type="modules" name="position-2" style="none" />
+					<!-- End Content -->
+				</main>
+				<?php if ($position7ModuleCount) : ?>
+					<div id="aside" class="span3">
+						<!-- Begin Right Sidebar -->
+						<jdoc:include type="modules" name="position-7" style="well" />
+						<!-- End Right Sidebar -->
+					</div>
+				<?php endif; ?>
+			</div>
+		</div>
+	</div>
+	<!-- Footer -->
+	<jdoc:include type="modules" name="debug" style="none" />
+</body>
+</html>
